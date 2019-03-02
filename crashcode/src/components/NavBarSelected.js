8d96cf0c2d59d458837e659e982f669413042e41
ceb3745 <ceb3745@g.rit.edu>
2019-03-02 17:20:47 -0500

    added in the new background image

--- crashcode/src/components/NavBarSelected.js
+++ crashcode/src/components/NavBarSelected.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 
 class NavBarSelected extends Component {
+    constructor(props) {
+        super(props);
+
+        this.getLangExtension = this.getLangExtension.bind(this);
+    }
+
+
+    getLangExtension(){
+        if(this.props.language === 'python'){
+            return '.py';
+        }
+        if(this.props.language === 'java'){
+            return '.java';
+        }
+        if(this.props.language === 'javascript'){
+            return '.js';
+        }
+    }
 
     render() {
         return(
             <div className="navBarSelected">
-                <div>crash_code</div>
+                <div className="titleName">crash_code{this.getLangExtension()}</div>
                 <div onClick={(e) => this.props.home(e)} className="navLink">Home</div>
                 <div onClick={(e) => this.props.glossary(e)} className="navLink">Glossary</div>
                 <div onClick={(e) => this.props.about(e)} className="navLink">About</div>
