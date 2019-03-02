cd3f1f9c8111c75d3adb354418875b971e9db7b0
clairechen <cxc2230@rit.edu>
2019-03-02 17:50:36 -0500

    Added some text to About and Glossary pages

--- crashcode/src/components/About.js
+++ crashcode/src/components/About.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 
 class About extends Component{
-
-
     render(){
         return(
             <div className="contentPage">
                 <div className="title">
                     <h1> This is the about. </h1>
+                    <h2> Resources: </h2>
+                    <ul>
+                    <li>Paiza API </li>
+                    </ul>
                 </div>
             </div>
         );
--- crashcode/src/components/Glossary.js
+++ crashcode/src/components/Glossary.js
@@ -7,7 +7,19 @@ class Glossary extends Component{
         return(
             <div className="contentPage">
                 <div className="title">
-                    <h1> This is the glossary. </h1>
+                    <h1> Glossary </h1>
+                    <h2> Variable </h2>
+                    <p> Used to store values. </p>
+                    <p>Pro Tip: variable names should make sense to what they are holding </p>
+                    <h2> Loops </h2>
+                    <p> A sequence of instructions that are continuously repeated until a certain condition is reached </p>
+                    <h2> Conditionals </h2>
+                    <p> Performing actions based on certain states. This follows an if... then... format</p>
+                    <h2> Operators </h2>
+                    <p> The action to be done on values (numbers, booleans, etc.) or comparing values (equal to, not equal to, etc.) </p>
+                    <h2> Methods </h2>
+                    <p>A block of statements to be ran. </p>
+                    <p> Pro Tip: Methods should only do one specific thing </p>
                 </div>
             </div>
         );
