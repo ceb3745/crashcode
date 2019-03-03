39de99b31cb8d1f33430240700b81ae99759a96d
ceb3745 <ceb3745@g.rit.edu>
2019-03-02 22:40:04 -0500

    added some stickers and pencil

--- crashcode/src/components/Home.js
+++ crashcode/src/components/Home.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import Heart from '../assets/heart.png';
+import Lightning from '../assets/lightning.png';
+import Lizard from '../assets/lizard.png';
+import Pencil from '../assets/pencil.png';
 
 class Home extends Component{
 
@@ -13,6 +17,10 @@ class Home extends Component{
                 <h1 className="welcome"> Welcome to crash_code. </h1>
                 </div>
                 <h2>Today I'm learning:</h2><h2>{this.props.language}</h2>
+                <img src={Heart} className="stickerHeart"/>
+                <img src={Lightning} className="stickerLightning"/>
+                <img src={Lizard} className="stickerLizard"/>
+                <img src={Pencil} className="pencil"/>
             </div>
         );
     }
